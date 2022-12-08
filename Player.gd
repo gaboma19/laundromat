88611a8b91002d6d8828edfcf638f1986aec4d3b
Gabriel Ma <gaboma19@gmail.com>
2022-12-08 11:47:57 -0500

    12-8

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ export var speed = 400
 var screen_size
 var velocity
 var direction
+onready var animation = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +15,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity = Vector2.ZERO # The player's movement vector.
-	$AnimatedSprite.play()
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -28,33 +28,16 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 
-	move_and_collide(velocity * delta)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-
+	
 	if velocity == Vector2.ZERO:
-		if direction == "down":
-			$AnimatedSprite.animation = "idle_down"
-		elif direction == "up":
-			$AnimatedSprite.animation = "idle_up"
-		elif direction == "left":
-			$AnimatedSprite.animation = "idle_left"
-		elif direction == "right":
-			$AnimatedSprite.animation = "idle_right"
-		else:
-			$AnimatedSprite.animation = "idle_down"
-	elif velocity.x > 0:
-		$AnimatedSprite.animation = "walk_right"
-		direction = "right"
-	elif velocity.x < 0:
-		$AnimatedSprite.animation = "walk_left"
-		direction = "left"
-	elif velocity.y < 0:
-		$AnimatedSprite.animation = "walk_up"
-		direction = "up"
-	elif velocity.y > 0:
-		$AnimatedSprite.animation = "walk_down"
-		direction = "down"
+		$AnimationTree.get("parameters/playback").travel("idle")
+	else:
+		$AnimationTree.get("parameters/playback").travel("walk")
+		$AnimationTree.set("parameters/idle/blend_position", velocity)
+		$AnimationTree.set("parameters/walk/blend_position", velocity)
+		move_and_collide(velocity * delta)
 		
 func _input(event):
 	if event.is_action_pressed("interact"):
